6767cd3a12a23dc5f7fc027fad62d9195ff5d663
Andrei-Agmata <aagmata@myseneca.ca>
2021-12-01 23:25:33 -0500

    commit

--- data-service-auth.js
+++ data-service-auth.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 let Schema = mongoose.Schema;
 
 var userSchema = new Schema({
@@ -29,18 +30,27 @@ module.exports.initialize = function () {
 module.exports.registerUser = function (userData) {
   return new Promise(function (resolve, reject) {
     if (userData.password == userData.password2) {
-      let newUser = new User(userData);
-      newUser.save(function (err) {
-        if (err) {
-          if (err.code == 11000) {
-            reject("User Name already taken");
-          } else {
-            reject("There was an error creating the user: " + err);
-          }
-        } else {
-          resolve();
-        }
-      });
+      bcrypt
+        .genSalt(10) // Generate a "salt" using 10 rounds
+        .then(salt => bcrypt.hash(userData.password, salt)) // encrypt the password: "myPassword123"
+        .then(hash => {
+          // TODO: Store the resulting "hash" value in the DB
+          userData.password = hash;
+          let newUser = User(userData);
+          newUser
+            .save()
+            .then(() => {
+              resolve();
+            })
+            .catch(err => {
+              if (err.code == 11000) reject("user name already taken");
+              else if (err.code != 11000)
+                reject(`There was an error creating the user: ${err}`);
+            });
+        })
+        .catch(err => {
+          reject("There was an error encrypting the password!"); // Show any errors that occurred during the process
+        });
     } else {
       reject("Passwords do not match");
     }
@@ -54,26 +64,31 @@ module.exports.checkUser = function (userData) {
       .then(users => {
         if (users.length == 0) {
           reject(`Unable to find user: ${userData.userName}`);
-        } else if (users[0].password !== userData.password) {
-          reject(`Incorrect Password for user: ${userData.userName}`);
-        } else if (users[0].password == userData.password) {
-          users[0].loginHistory.push({
-            dateTime: new Date().toString(),
-            userAgent: userData.userAgent,
-          });
-          User.updateOne(
-            { userName: users[0].userName },
-            { $set: { loginHistory: users[0].loginHistory } },
-            { multi: false }
-          )
-            .exec()
-            .then(() => {
-              resolve(users[0]);
-            })
-            .catch(err => {
-              reject(`There was an error verifying the user: ${err}`);
-            });
         }
+
+        bcrypt.compare(userData.password, users[0].password).then(result => {
+          if (result === true) {
+            users[0].loginHistory.push({
+              dateTime: new Date().toString(),
+              userAgent: userData.userAgent,
+            });
+
+            User.updateOne(
+              { userName: users[0].userName },
+              { $set: { loginHistory: users[0].loginHistory } },
+              { multi: false }
+            )
+              .exec()
+              .then(() => {
+                resolve(users[0]);
+              })
+              .catch(err => {
+                reject(`There was an error verifying the user: ${err}`);
+              });
+          } else {
+            reject(`Incorrect Password for user: ${userData.userName}`);
+          }
+        });
       })
       .catch(err => {
         reject(`Unable to find user: ${userData.userName}`);
